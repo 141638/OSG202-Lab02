5b05d8ee2921f3b5c3fed088147f939ba03f3aaa
141638 <55794812+141638@users.noreply.github.com>
2020-03-11 09:48:24 +0700

    Create Ex1.sh

---
+++ Ex1.sh
@@ -0,0 +1,12 @@
+echo -n "Enter name : "
+read name
+echo "Enter program: "
+read prog
+echo "Enter enrollment: "
+read enrol
+
+echo "Your name: $name"
+echo "Enrollment: $enrol"
+echo "Working : $prog"
+
+exit 0
